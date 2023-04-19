5672869e2b70aa773ad5b941dc6063369475980b
Vojtech Juranek <vojtech.juranek@gmail.com>
2023-04-19 13:21:43 +0200

    mnist: imporove python script

--- mnist/mnist2sql.py
+++ mnist/mnist2sql.py
@@ -6,30 +6,42 @@ import struct
 MNIST_LABEL_MAGIC = 2049
 MNIST_IMAGE_MAGIC = 2051
 
-CREATE_STMT = "CREATE TABLE {}(id SERIAL NOT NULL PRIMARY KEY,  label SMALLINT, pixels BYTEA);\n"
+CREATE_STMT = ("CREATE TABLE {}(id SERIAL NOT NULL PRIMARY KEY, "
+               "label SMALLINT, pixels BYTEA);\n")
 INSERT_STMT = "INSERT INTO {}(label, pixels) VALUES({}, '\\x{}');\n"
 
-def prepare_sql(label_file, img_file, sql_file, table_name):
-    with open(sql_file, 'w') as f:
-        f.write(CREATE_STMT.format(table_name))
 
-        with gzip.open(label_file, "rb") as fl:
-            with gzip.open(img_file, "rb") as fi:
-                magic_label, num_label = struct.unpack(">II", fl.read(8))
-                magic_img, num_img, rows, columns = struct.unpack(">IIII", fi.read(16))
- 
+def prepare_sql(label_path, img_path, sql_path, table_name):
+    """
+    Combines label file with images from the other file and create new file
+    with SQL commands for creating and populating specified DB table with
+    these values. Images bytes are encoded as HEX strings.
+    """
+    with open(sql_path, mode='w', encoding="utf-8") as sql_file:
+        sql_file.write(CREATE_STMT.format(table_name))
+
+        with gzip.open(label_path, "rb") as label_file:
+            with gzip.open(img_path, "rb") as image_file:
+                magic_label, num_label = struct.unpack(
+                    ">II", label_file.read(8))
+                magic_img, num_img, rows, columns = struct.unpack(
+                    ">IIII", image_file.read(16))
+
                 if MNIST_LABEL_MAGIC != magic_label:
-                    raise ValueError("Unexpected label magic: %r" % magic_label)
+                    raise ValueError("Unexpected label magic: {magic_label}")
                 if MNIST_IMAGE_MAGIC != magic_img:
-                    raise ValueError("Unexpected image magic: %r" % magic_img)
+                    raise ValueError("Unexpected image magic: {magic_img}")
                 if num_label != num_img:
-                    raise ValueError("Size of labels (%r) doesn't match with size of image sample (%r)" % (num_label, num_img))
+                    raise ValueError(
+                        "Size of labels {num_label} doesn't match with size of"
+                        " image sample {num_img}")
 
                 img_size = rows * columns
-                for i in range(0, num_label):
-                    label = struct.unpack(">b", fl.read(1))
-                    pixels = fi.read(img_size)
-                    f.write(INSERT_STMT.format(table_name, label[0], pixels.hex()))
+                for _ in range(0, num_label):
+                    label = struct.unpack(">b", label_file.read(1))
+                    pixels = image_file.read(img_size)
+                    sql_file.write(
+                        INSERT_STMT.format(table_name, label[0], pixels.hex()))
 
 prepare_sql(
     "../data/train-labels-idx1-ubyte.gz",
@@ -44,4 +56,3 @@ prepare_sql(
     "postgres/mnist-test.sql",
     "mnist_test"
 )
-
