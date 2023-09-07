8c442426231ce7636c513278fac659d4711e5806
Vojtech Juranek <vojtech.juranek@gmail.com>
2023-09-07 11:19:24 +0200

    iris: improve script for preparing sql files

--- iris/iris2sql.py
+++ iris/iris2sql.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
-import pandas as pd
+import numpy as np
+from sklearn import datasets
+from sklearn import model_selection
 
+TRAIN_FILE = "./postgres/iris_train.sql"
+TEST_FILE = "./postgres/iris_test.sql"
+
+TRAIN_TABLE = "iris_train"
+TEST_TABLE = "iris_test"
 COLUMNS = [
     "sepal_length",
     "sepal_width",
@@ -9,16 +16,26 @@ COLUMNS = [
     "petal_width",
     "iris_class",
 ]
+CREATE_STMT = "CREATE TABLE {}(id SERIAL NOT NULL PRIMARY KEY,"\
+    " {} float, {} float, {} float, {} float, {} int);\n"
+INSERT_STMT = "INSERT INTO {}({}, {}, {}, {}, {}) "\
+    "VALUES({}, {}, {}, {}, {});\n"
 
-CREATE_STMT = "CREATE TABLE {}(id SERIAL NOT NULL PRIMARY KEY, {} float, {} float, {} float, {} float, {} varchar(32));"
-INSERT_STMT = "INSERT INTO {}({}, {}, {}, {}, {}) VALUES({}, {}, {}, {}, '{}');\n"
-
-df = pd.read_csv("./iris.data")
-
-def prepare_sql(create_stmt, data, file_name, table_name):
+def prepare_sql(data, file_name, table_name):
     with open(file_name, 'w') as f:
-        f.write(create_stmt.format(table_name, *COLUMNS))
-        for row in data.values:
+        f.write(CREATE_STMT.format(table_name, *COLUMNS))
+        for row in data:
             f.write(INSERT_STMT.format(table_name, *COLUMNS, *row))
 
-prepare_sql(CREATE_STMT, df, "iris.sql", "iris")
+iris = datasets.load_iris()
+iris_merged = np.column_stack((iris.data, iris.target))
+
+# convert labels from float to int
+iris_merged = iris_merged.astype('object')
+iris_merged[:, 4] = iris_merged[:, 4].astype(int)
+
+train, test = model_selection.train_test_split(
+    iris_merged, test_size=0.08)
+
+prepare_sql(train, TRAIN_FILE, TRAIN_TABLE)
+prepare_sql(test, TEST_FILE, TEST_TABLE)
